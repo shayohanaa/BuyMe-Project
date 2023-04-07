159f4c29cf4226ef0ecd6059e25192f9609e8e8e
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2023-04-07 19:39:31 +0300

    Update test_BuyMe.py

---
+++ test_BuyMe.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from homepage import HomePage
+from homepage import Login
+from homepage import Extra
+import json
+
+
+class setUp(TestCase):
+    def setUp(self):
+        json_file = open('config.json', 'r')
+        data = json.load(json_file)
+        browser = data['browserType']
+        if browser == 'chrome':
+            self.driver = webdriver.Chrome(service=Service('C://Users/shayo/chromedriver_win32/chromedriver.exe'))
+        elif browser == 'firefox':
+            self.driver = webdriver.Firefox(service=Service('FIREFOXDRIVER_PATH'))
+
+        url = data['url']
+        self.driver.get(url)
+
+        self.homepage = HomePage(self.driver)
+        self.login = Login(self.driver)
+        self.extras = Extra(self.driver)
+
+    def test_BuyMe(self):
+        # self.homepage.signUp()
+        # self.login.Login()
+        self.login.fulfillGift()
+        self.login.Gift()
+        # self.extras.Loading()
+        # self.extras.errorMessage()
+        # self.extras.giftScreen()
+        # self.extras.info_Screen()
+
+    def tearDown(self):
+        self.driver.quit()

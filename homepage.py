6e8a6298cf0db65dbeb0a9e220941f256feea4ba
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2023-04-12 00:36:36 +0300

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -42,13 +42,13 @@ class Login(BasePage):
         self.driver.get(url)
 
     def Login(self):
-        self.click_element(By.XPATH, '//*[@id="ember1006"]/div/ul[1]/li[3]/a')
-        self.enter_text(By.ID, 'ember1852', Constants.User)
-        self.enter_text(By.ID, 'ember1859', Constants.Pass)
-        self.click_element(By.ID, 'ember1868')
+        self.click_element(By.XPATH, '//header/div[1]/div/ul[1]/li[3]')
+        self.enter_text(By.CSS_SELECTOR, 'input[type=email]', Constants.User)
+        self.enter_text(By.CSS_SELECTOR, 'input[type=password]', Constants.Pass)
+        self.click_element(By.CSS_SELECTOR, 'button[type=submit]')
 
     def fulfillGift(self):
-        self.wait(By.ID, 'ember1053', 10)
+        self.wait(By.ID, 'ember1053', 15)
         self.click_element(By.ID, 'ember1053')
         WebDriverWait(self.driver, 5).until(
             expected_conditions.element_to_be_clickable((By.ID, "ember1076")))
@@ -70,8 +70,8 @@ class Login(BasePage):
     def Gift(self):
         self.wait(By.PARTIAL_LINK_TEXT, 'Claro', 10)
         allure.attach(self.driver.get_screenshot_as_png(), name="Type", attachment_type=allure.attachment_type.PNG)
-        # get_url = self.driver.current_url
-        # assert get_url == "https://buyme.co.il/search?category=438&region=11"
+        get_url = self.driver.current_url
+        assert get_url == "https://buyme.co.il/search?category=438&region=11"
         self.click_element(By.PARTIAL_LINK_TEXT, "Claro")
         self.driver.implicitly_wait(2)
         self.enter_text(By.CSS_SELECTOR, 'input[inputmode=decimal]', '150')
@@ -80,48 +80,75 @@ class Login(BasePage):
         self.click_element(By.XPATH, '//form/div[1]/div/div/div/div/div[1]')
         self.enter_text(By.ID, 'friendName', 'John Doe')
         self.click_element(By.CLASS_NAME, 'selected-name')
-        time.sleep(3)
         self.driver.implicitly_wait(2)
-        # self.wait(By.CSS_SELECTOR, 'li[value=10]', 10)
+        WebDriverWait(self.driver, 2).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, "//form/div[2]/div[2]/label/div/div[2]/ul/li[2]")))
         self.click_element(By.XPATH, '//form/div[2]/div[2]/label/div/div[2]/ul/li[2]')
         self.scroll_down_end()
         self.clear_text(By.XPATH, '//form/div[2]/div[4]/label/textarea')
         self.enter_text(By.XPATH, '//form/div[2]/div[4]/label/textarea', 'Happy birthday.')
+        time.sleep(2)
+        self.driver.find_element(By.CSS_SELECTOR, "input[name=logo]").send_keys("C:\\Users\\shayo\\passover.png")
+        time.sleep(2)
         allure.attach(self.driver.get_screenshot_as_png(), name="Blessings", attachment_type=allure.attachment_type.PNG)
-#####
-        # self.click_element(By.XPATH, '//form/div[2]/div[5]/div[2]/div[1]/label')
-        # time.sleep(1)
-        # self.upload_File(By.XPATH, '//form/div[2]/div[5]/div[2]/div[1]/label', "C:/Users/shayo/passover.jpg")
-        # time.sleep(1)
-#####
+        self.driver.implicitly_wait(5)
         self.click_element(By.CSS_SELECTOR, 'button[type=submit]')
-        # self.click_element(By.ID, 'ember3380')
+        self.click_element(By.CSS_SELECTOR, 'div[gtm=עכשיו')
         self.click_element(By.CSS_SELECTOR, 'svg[gtm=method-email')
         self.enter_text(By.CSS_SELECTOR, 'input[name=email]', 'JohnDoe@gmail.com')
         self.scroll_down_end()
         self.enter_text(By.CSS_SELECTOR, 'input[type=text]', 'John Doe')
         allure.attach(self.driver.get_screenshot_as_png(), name="Receiver", attachment_type=allure.attachment_type.PNG)
-        time.sleep(3)
-
-        # receiver = self.find_element(By.CSS_SELECTOR, 'input[type=text]')
-        # assert receiver == "John Doe"
+        self.assert_Text(By.CSS_SELECTOR, 'input[type=text]', 'John Doe')
+        self.driver.refresh()
 
 
-class Extras(BasePage):
+class Extra(BasePage):
     def __init__(self, driver):
         BasePage.__init__(self, driver)
+        # json_file = open('config.json', 'r')
+        # data = json.load(json_file)
+        # url = data['url']
+        # self.driver.get(url)
 
     def Loading(self):
-        json_file = open('config.json', 'r')
-        data = json.load(json_file)
-        url = data['url']
-        self.driver.get(url)
-
         element = self.driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div')
-
         height = element.size['height']
         width = element.size['width']
-
         print(f'\nElement height: {height}px')
         print(f'Element width: {width}px')
 
+    def errorMessage(self):
+        self.wait(By.XPATH, '//header/div[1]/div/ul[1]/li[3]', 5)
+        self.click_element(By.XPATH, '//header/div[1]/div/ul[1]/li[3]')
+        self.click_element(By.XPATH, '//form/button')
+        self.assert_Text(By.CLASS_NAME, 'parsley-required', 'כל המתנות מחכות לך! אבל קודם צריך מייל וסיסמה')
+
+    def giftScreen(self):
+        self.driver.get("https://buyme.co.il")
+        self.wait(By.ID, 'ember1053', 10)
+        self.click_element(By.ID, 'ember1053')
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.element_to_be_clickable((By.ID, "ember1076")))
+        # self.wait(By.ID, "ember1076", 5)
+        self.click_element(By.ID, 'ember1076')
+
+        self.click_element(By.ID, 'ember1088')
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.element_to_be_clickable((By.ID, 'ember1111')))
+        # self.wait(By.ID, "ember1111", 5)
+        self.click_element(By.ID, 'ember1111')
+
+        self.click_element(By.ID, 'ember1120')
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.element_to_be_clickable((By.ID, "ember1171")))
+        # self.wait(By.ID, "ember1171", 5)
+        self.click_element(By.ID, 'ember1171')
+        self.scroll_down_end()
+        allure.attach(self.driver.get_screenshot_as_png(), name="giftScreen", attachment_type=allure.attachment_type.PNG)
+
+    def info_Screen(self):
+        self.driver.get("https://buyme.co.il/money/1229712?price=150")
+        element = self.driver.find_element(By.CLASS_NAME, 'bottom-xs')
+        color = element.value_of_css_property('color')
+        print("The element color is: ", color)

20c63c80ba63321c6b457e85c0e8cc658e985d98
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2023-04-12 19:00:17 +0300

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -48,6 +48,7 @@ class Login(BasePage):
         self.click_element(By.CSS_SELECTOR, 'button[type=submit]')
 
     def fulfillGift(self):
+        self.wait(By.TAG_NAME, 'body', 15)
         self.wait(By.ID, 'ember1053', 15)
         self.click_element(By.ID, 'ember1053')
         WebDriverWait(self.driver, 5).until(
@@ -71,7 +72,8 @@ class Login(BasePage):
         self.wait(By.PARTIAL_LINK_TEXT, 'Claro', 10)
         allure.attach(self.driver.get_screenshot_as_png(), name="Type", attachment_type=allure.attachment_type.PNG)
         get_url = self.driver.current_url
-        assert get_url == "https://buyme.co.il/search?category=438&region=11"
+        print(get_url)
+        assert get_url == "https://buyme.co.il/search?budget=2&category=438&region=13"
         self.click_element(By.PARTIAL_LINK_TEXT, "Claro")
         self.driver.implicitly_wait(2)
         self.enter_text(By.CSS_SELECTOR, 'input[inputmode=decimal]', '150')
@@ -80,16 +82,13 @@ class Login(BasePage):
         self.click_element(By.XPATH, '//form/div[1]/div/div/div/div/div[1]')
         self.enter_text(By.ID, 'friendName', 'John Doe')
         self.click_element(By.CLASS_NAME, 'selected-name')
-        self.driver.implicitly_wait(2)
         WebDriverWait(self.driver, 2).until(
             expected_conditions.element_to_be_clickable((By.XPATH, "//form/div[2]/div[2]/label/div/div[2]/ul/li[2]")))
         self.click_element(By.XPATH, '//form/div[2]/div[2]/label/div/div[2]/ul/li[2]')
         self.scroll_down_end()
         self.clear_text(By.XPATH, '//form/div[2]/div[4]/label/textarea')
         self.enter_text(By.XPATH, '//form/div[2]/div[4]/label/textarea', 'Happy birthday.')
-        time.sleep(2)
         self.driver.find_element(By.CSS_SELECTOR, "input[name=logo]").send_keys("C:\\Users\\shayo\\passover.png")
-        time.sleep(2)
         allure.attach(self.driver.get_screenshot_as_png(), name="Blessings", attachment_type=allure.attachment_type.PNG)
         self.driver.implicitly_wait(5)
         self.click_element(By.CSS_SELECTOR, 'button[type=submit]')
@@ -99,17 +98,17 @@ class Login(BasePage):
         self.scroll_down_end()
         self.enter_text(By.CSS_SELECTOR, 'input[type=text]', 'John Doe')
         allure.attach(self.driver.get_screenshot_as_png(), name="Receiver", attachment_type=allure.attachment_type.PNG)
-        self.assert_Text(By.CSS_SELECTOR, 'input[type=text]', 'John Doe')
-        self.driver.refresh()
+        # self.assert_Text(By.CSS_SELECTOR, 'input[type=text]', 'John Doe')
+        self.driver.get("https://buyme.co.il")
 
 
 class Extra(BasePage):
     def __init__(self, driver):
         BasePage.__init__(self, driver)
-        # json_file = open('config.json', 'r')
-        # data = json.load(json_file)
-        # url = data['url']
-        # self.driver.get(url)
+        json_file = open('config.json', 'r')
+        data = json.load(json_file)
+        url = data['url']
+        self.driver.get(url)
 
     def Loading(self):
         element = self.driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div')
@@ -119,14 +118,15 @@ class Extra(BasePage):
         print(f'Element width: {width}px')
 
     def errorMessage(self):
-        self.wait(By.XPATH, '//header/div[1]/div/ul[1]/li[3]', 5)
+        self.wait(By.XPATH, '//header/div[1]/div/ul[1]/li[3]', 10)
         self.click_element(By.XPATH, '//header/div[1]/div/ul[1]/li[3]')
         self.click_element(By.XPATH, '//form/button')
         self.assert_Text(By.CLASS_NAME, 'parsley-required', 'כל המתנות מחכות לך! אבל קודם צריך מייל וסיסמה')
 
     def giftScreen(self):
         self.driver.get("https://buyme.co.il")
-        self.wait(By.ID, 'ember1053', 10)
+        self.wait(By.TAG_NAME, 'body', 15)
+        self.wait(By.ID, 'ember1053', 15)
         self.click_element(By.ID, 'ember1053')
         WebDriverWait(self.driver, 5).until(
             expected_conditions.element_to_be_clickable((By.ID, "ember1076")))
